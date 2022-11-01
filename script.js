c6c6e30ba9e70d0e0663253a29ea22291ef0c6e0
DeiverA <117132056+DeiverA@users.noreply.github.com>
2022-10-31 20:01:38 -0600

    Add files via upload
    Version 1

---
+++ script.js
@@ -0,0 +1,19 @@
+let num1
+let num2
+let operator
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+
+});
+
+readline.question('Quiere hacer un calculo?', decision =>{
+    console.log(`Cual es el primer numero ${decision}!`);
+    realine.close();
+})
+
+while (decision="yes") {
+    if 
+    
+}
+ 
